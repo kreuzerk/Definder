b3b2126a1abd6baa79a8111c6aa1e7371eeff845
kreuzerk <kevin.kreuzer@hotmail.com>
2016-06-25 12:59:05 +0200

    Style the text of the user component

--- src/app/components/welcome-user/welcome.user.component.ts
+++ src/app/components/welcome-user/welcome.user.component.ts
@@ -3,13 +3,19 @@ import {Component} from 'angular2/core';
 @Component({
   selector: 'welcome-user',
   template: `
-    <h2>Willkommen</h2>
+    <h2>Welcome</h2>
     <img [src]="image"/>
   `,
   styles: [
     `
     h2{
       display: inline;
+      font-family: "Times New Roman", Times, serif;
+      margin-top: 2cm;
+      font-size : 14pt;
+      line-height : 100%;
+      margin : 10px;
+      color : #909090;
     }
     `
   ]
