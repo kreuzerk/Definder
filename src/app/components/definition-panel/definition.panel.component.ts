e561e260c18d565068444d6a7231fcf4091d7d29
kreuzerk <kevin.kreuzer@hotmail.com>
2016-06-27 09:17:36 +0200

    Add delete function to the ui

--- src/app/components/definition-panel/definition.panel.component.ts
+++ src/app/components/definition-panel/definition.panel.component.ts
@@ -9,18 +9,28 @@ import {Component, Input} from '@angular/core';
     </div>
     <div class="panel-body">
       <ul>
-        <li *ngFor="#definition of definitions">
+        <li *ngFor="let definition of definitions; let i = index">
+        <img [src]="image" (click)="deleteDefinition(i)"/>
           <b>{{definition.headword}}</b>
           {{definition.senses[0].definition}}
         </li>
       </ul>
     </div>
   </div>
-  `
+  `,
+  styles: [`
+    ul{
+      list-style-type: none;
+    }
+    `]
 })
 export class DefinitionPanelComponent{
 
   @Input() definitions: any;
   @Input() title: string;
+  image: string = 'build/' + require('./trash-icon.png');
 
+  deleteDefinition(index: number): void{
+    this.definitions.splice(index, 1);
+  }
 }
